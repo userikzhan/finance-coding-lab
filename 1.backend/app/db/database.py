10d9c9012fee2d6c31af69e8113382f32a1f7162
userikzhan <userikzhan@gmail.com>
2026-05-13 15:40:21 +0500

    update 1.backend\app\database.py

--- 1.backend/app/db/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://user:password@db:5432/finance"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
