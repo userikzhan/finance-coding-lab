2661312b3af84ee1453fbbfb3ea4cf8b3e0947f8
userikzhan <userikzhan@gmail.com>
2026-05-13 15:43:09 +0500

    update 1.backend\app\models\user.py

---
+++ 1.backend/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.database import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    email = Column(String, unique=True, index=True)
+
+    password = Column(String)
+
+    role = Column(String, default="user")
