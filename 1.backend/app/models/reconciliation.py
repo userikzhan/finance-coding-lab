e43aa9ef3b3fdc618176e8bbe870101373c6d2a2
userikzhan <userikzhan@gmail.com>
2026-05-13 21:21:29 +0500

    update 1.backend\app\models\reconciliation.py

---
+++ 1.backend/app/models/reconciliation.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.db.base import Base
+
+
+class Reconciliation(Base):
+
+    __tablename__ = "reconciliations"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    file_name = Column(String)
