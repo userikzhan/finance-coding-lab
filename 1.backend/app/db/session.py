6e151fae2a803143855da393c4c06326682f7201
userikzhan <userikzhan@gmail.com>
2026-05-15 01:45:05 +0500

    add comments to 1.backend\app\db\session.py

--- 1.backend/app/db/session.py
+++ 1.backend/app/db/session.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# URL подключения к PostgreSQL
+# localhost используется потому что:
+# - Alembic запускается из Windows
+# - Docker пробрасывает порт 5433 наружу
 DATABASE_URL = "postgresql://user:password@localhost:5433/finance"
 
-engine = create_engine(DATABASE_URL)
+# Создаём SQLAlchemy engine
+engine = create_engine(
+    DATABASE_URL
+)
 
+# Создаём фабрику сессий
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
