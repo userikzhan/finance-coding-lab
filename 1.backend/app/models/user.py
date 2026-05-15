89da9357345a4041edfcd61413180bd544230f4c
userikzhan <userikzhan@gmail.com>
2026-05-15 17:45:47 +0500

    correct 1.backend\app\models\user.py

--- 1.backend/app/models/user.py
+++ 1.backend/app/models/user.py
@@ -11,6 +11,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    email = Column(String, unique=True, index=True)
-
-    password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
