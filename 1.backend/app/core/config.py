5bb91468bcd477f7fc08f871726439322aed179d
userikzhan <userikzhan@gmail.com>
2026-05-17 00:15:56 +0500

    update 1.backend\app\core\config.py

--- 1.backend/app/core/config.py
+++ 1.backend/app/core/config.py
@@ -3,13 +3,18 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
+    # PostgreSQL
     DATABASE_URL: str
 
+    # JWT
     SECRET_KEY: str
 
-    ALGORITHM: str
+    ALGORITHM: str = "HS256"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # Refresh token
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
 
     class Config:
         env_file = ".env"
